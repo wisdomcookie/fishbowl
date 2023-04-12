b3653df3763b91d36210fe2502eb03bf254b0a48
Nathan_Dinh <dinhtrongnhan2015@gmail.com>
2023-04-11 23:29:43 -0400

    Add some group helper headers

--- groups/group.h
+++ groups/group.h
@@ -1,21 +1,37 @@
 #ifndef GROUP_H
 #define GROUP_H
-
-
+#include "../admin/administrator.h"
+#include "../profiles/aquarium.h"
+#include "../profiles/person.h"
+#include "../comm/post.h"
 #include <string>
 #include <vector>
+
 class Group
 {
 public:
     Group();
     Group(std::string name);
 
+    ~Group();
+
+    void add_member();
+    void remove_member(Person *profile);
+
+    std::string get_name();
+    std::string get_description();
+    std::string get_admin();
+    std::string get_members();
+    Aquarium get_aquarium();
+
 
 private:
     std::string name;
     std::string description;
-    std::vector<Administrator> admin;
-    std::vector<Profile> members;
+    std::vector<Administrator*> admin;
+    std::vector<Person*> members;
+    std::vector<Post*> postHistory;
+    Aquarium aquarium;
 
 };
 
--- groups/groups.pro
+++ groups/groups.pro
@@ -15,9 +15,11 @@ SOURCES += ../profiles/profile.cpp
 SOURCES += ../profiles/person.cpp
 SOURCES += ../profiles/fish.cpp
 SOURCES += ../profiles/aquarium.cpp
+SOURCES += ../comm/post.cpp
 
 HEADERS += ../admin/administrator.h
 HEADERS += ../profiles/profile.h
 HEADERS += ../profiles/person.h
 HEADERS += ../profiles/fish.h
 HEADERS += ../profiles/aquarium.h
+HEADERS += ../comm/post.h
